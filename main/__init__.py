e3b046ccb635acd00ea826d3b9de1a8fd53cf17a
SiddhuDB <96898865+SiddhuDB@users.noreply.github.com>
2023-01-29 15:19:56 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", 17998867, cast=int)
-API_HASH = config("API_HASH", f66de5165eaa46f7f4cc5387b500d91d)
-BOT_TOKEN = config("BOT_TOKEN", 5436556142:AAE8G9FXPdSpm-Y3pFjX8NiygSlpn8qnsx0)
+API_ID = config("API_ID",17998867, cast=int)
+API_HASH = config("API_HASH",f66de5165eaa46f7f4cc5387b500d91d)
+BOT_TOKEN = config("BOT_TOKEN",5436556142:AAE8G9FXPdSpm-Y3pFjX8NiygSlpn8qnsx0)
 SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+AUTH = config("AUTH",5205471376, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
